683b2b85d3f642e7e7db4922d7c663cc83abfd35
jeonsang-eon <tkd0204@naver.com>
2024-01-31 20:46:07 +0900

    Initial commit

---
+++ src/main/java/com/example/docker/entity/Member.java
@@ -0,0 +1,14 @@
+package com.example.docker.entity;
+
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+
+//@Entity
+public class Member {
+    //@Id
+    private int id;
+
+    //@Column
+    private String name;
+}
